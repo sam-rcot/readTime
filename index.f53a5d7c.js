5b028eddaf10b7cda132535439a6830dced2073b
Sam Liburd <sam.liburd@rcot.co.uk>
2024-07-10 11:17:02 +0100

    Updates

---
+++ index.f53a5d7c.js
@@ -0,0 +1,2 @@
+const t=window.parent.document.querySelector(".article").textContent,e=document.querySelector(".textContainer"),n=Math.round([...t.matchAll(/[^\s]+/g)].length/200);e.textContent=`\u{23F1}\u{FE0F} ${n} min read`;
+//# sourceMappingURL=index.f53a5d7c.js.map
